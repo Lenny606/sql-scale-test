df8060c80157d9294ab922ca3ea091ef0e6db472
tomas.kravcik <thomas.kravcik@gmail.com>
2026-04-29 20:51:32 +0200

    feat: add python script to generate mock csv datasets for users and comments

---
+++ generator.py
@@ -0,0 +1,27 @@
+import csv
+from faker import Faker
+import random
+
+fake = Faker()
+num_users = 10000
+num_posts = 50000
+num_comments = 1000000 # Změň na 10M pro opravdový test
+
+# Generujeme uživatele
+with open('users.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    for i in range(1, num_users + 1):
+        writer.writerow([i, fake.user_name(), fake.date_this_decade()])
+
+# Generujeme komentáře
+with open('comments.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    for i in range(1, num_comments + 1):
+        writer.writerow([
+            i, 
+            random.randint(1, num_posts), # Náhodné ID příspěvku
+            random.randint(1, num_users), # Náhodné ID uživatele
+            fake.sentence(),              # Text komentáře
+            fake.date_this_minute()       # Čas
+        ])
+print("CSV soubory vygenerovány!")

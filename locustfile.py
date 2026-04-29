c929067aafb5de56b3d82c086adb276cd9d27b7b
tomas.kravcik <thomas.kravcik@gmail.com>
2026-04-29 21:36:33 +0200

    refactor: update PostgresClient to use Locust 2.x event reporting and precision timing

--- locustfile.py
+++ locustfile.py
@@ -1,31 +1,39 @@
 import time
 import psycopg2
-from locust import User, task, between, events
+from locust import User, task, between
 
 class PostgresClient:
     """Jednoduchý klient, který loguje výsledky do Locustu"""
-    def __init__(self, host, database, user, password):
+    def __init__(self, host, database, user, password, environment):
         self.conn = psycopg2.connect(
             host=host, database=database, user=user, password=password
         )
         self.conn.autocommit = True
+        self.environment = environment
 
     def query(self, name, sql, params=None):
-        start_time = time.time()
+        start_time = time.perf_counter()
+        res = []
+        exception = None
         try:
             with self.conn.cursor() as cur:
                 cur.execute(sql, params)
-                res = cur.fetchall()
+                if cur.description:
+                    res = cur.fetchall()
         except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
-            events.request_failure.fire(
-                request_type="SQL", name=name, response_time=total_time, exception=e
-            )
-        else:
-            total_time = int((time.time() - start_time) * 1000)
-            events.request_success.fire(
-                request_type="SQL", name=name, response_time=total_time, response_length=len(res)
-            )
+            exception = e
+        
+        total_time = (time.perf_counter() - start_time) * 1000
+        
+        # Nový způsob logování v Locustu 2.x
+        self.environment.events.request.fire(
+            request_type="SQL",
+            name=name,
+            response_time=total_time,
+            response_length=len(res),
+            exception=exception,
+        )
+        return res
 
 class DatabaseUser(User):
     # Simulujeme náhodné čekání uživatele mezi akcemi
@@ -37,12 +45,13 @@ class DatabaseUser(User):
             host="localhost", 
             database="scaling_test", 
             user="user", 
-            password="password"
+            password="password",
+            environment=self.environment
         )
 
     @task(3)
     def heavy_aggregation(self):
-        # Tento dotaz bude bez indexů na 10M datech extrémně náročný
+        # Tento dotaz bude bez indexů na 1M datech náročný
         self.client.query(
             "count_top_users", 
             "SELECT username, COUNT(*) FROM users GROUP BY username HAVING COUNT(*) > 1 ORDER BY 2 DESC LIMIT 10;"
